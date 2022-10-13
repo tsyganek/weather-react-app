8366ba7de066ea12a459eaf42d7613ee2089638d
Kateryna Tsygankova <kateryna.tsygankova.se@gmail.com>
2022-10-13 21:39:12 +0300

    Footer and header edited

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,8 @@ export default function Footer() {
   return (
     <p>
       The project is designed by Kateryna Tsygankova, open-sourced on
-      <a href="#">GitHub</a>, and hosted on <a href="#">Netlify</a>
+      <a href="https://github.com/tsyganek/weather-react-app"> GitHub</a>, and
+      hosted on <a href="#">Netlify</a>
     </p>
   );
 }

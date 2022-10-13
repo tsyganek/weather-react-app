7074cf43f5844fadb4f8df7ae24442c90a5235fe
Kateryna Tsygankova <108408110+tsyganek@users.noreply.github.com>
2022-10-13 19:34:03 +0300

    Files added via upload

---
+++ src/Cities.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function Cities() {
+  return (
+    <div>
+      <ul>
+        <li>
+          <a href="#">Stockholm</a>
+        </li>
+        <li>
+          <a href="#">Kyiv</a>
+        </li>
+        <li>
+          <a href="#">Valencia</a>
+        </li>
+        <li>
+          <a href="#">Tokyo</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
---
+++ src/Weather.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Weather() {
+  return <h1>Hello work</h1>;
+}

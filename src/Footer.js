40c2d435336de590789306d333d337d6f7c187b9
Kateryna Tsygankova <kateryna.tsygankova.se@gmail.com>
2022-10-14 11:36:59 +0300

    links to footer added

--- src/Footer.js
+++ src/Footer.js
@@ -3,9 +3,14 @@ import React from "react";
 export default function Footer() {
   return (
     <p>
-      The project is designed by Kateryna Tsygankova, open-sourced on
+      The project is designed by{" "}
+      <a href="https://ua.linkedin.com/in/kateryna-tsygankova-15a429244?trk=public_profile_browsemap">
+        Kateryna Tsygankova
+      </a>
+      , open-sourced on
       <a href="https://github.com/tsyganek/weather-react-app"> GitHub</a>, and
-      hosted on <a href="#">Netlify</a>
+      hosted on{" "}
+      <a href="https://app.netlify.com/sites/playful-bonbon-32f07d/">Netlify</a>
     </p>
   );
 }
